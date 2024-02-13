d31f8ec9c6b07de13cacdf32966ca8f6fd5fe7bf
Chris McCoy <cmm@cisco.com>
2024-02-13 13:37:23 -0500

    Refactor output

--- output.tf
+++ output.tf
@@ -6,11 +6,11 @@
 
 output "cml2info" {
   value = {
-    address = module.ec2_instance.public_ip
+    address            = module.ec2_instance.public_ip
     deregister_license = "ssh -p1122 ${local.cfg.sys.user}@${module.ec2_instance.public_ip} /provision/del.sh"
-    url = "https://${local.lab_fqdn}"
-    cockpit = "https://${local.lab_fqdn}:9090"
-    cml_version = module.ready.state.version
-    breakout_tool = "https://${local.lab_fqdn}/breakout-docs/"
+    url                = "https://${local.lab_fqdn}"
+    cockpit            = "https://${local.lab_fqdn}:9090"
+    cml_version        = module.ready.state.version
+    breakout_tool      = "https://${local.lab_fqdn}/breakout-docs/"
   }
 }
