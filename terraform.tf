50de62391dc35e4c94c89f6865a57243dff50400
Chris McCoy <cmm@cisco.com>
2024-02-15 10:29:32 -0500

    Start using S3 as a backend

--- terraform.tf
+++ terraform.tf
@@ -11,5 +11,21 @@ terraform {
       version = "~>0.7.0"
     }
   }
+
   required_version = ">= 1.1.0"
+
+  backend "s3" {
+    bucket = "bah-cml-terraform-state"
+    key    = "cloud-cml/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "cml2" {
+  address        = "https://{local.lab_fqdn}:443"
+  username       = local.cfg.app.user
+  password       = module.secrets.conjur_secrets[local.cfg.app.pass]
+  use_cache      = false
+  skip_verify    = true
+  dynamic_config = true
 }
